82b7268147eb9e46c67121928e186935182fad0c
Alex Warden <146727849+AlexWarden2@users.noreply.github.com>
2023-12-20 17:07:22 +0000

    add nav links to hpme

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
     <>
       <h1>404: Page not found</h1>
+      <Link to="/">Home</Link>
     </>
 
   )
--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { Signup } from '../../components'
+import { Link } from 'react-router-dom';
 
 const SignupPage = () => {
   return (
-    <Signup />
+    <div>
+      <Signup />
+      <Link to="/">Back</Link>
+    </div>
   )
 }
 
