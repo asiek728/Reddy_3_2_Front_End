4eb3b924661e959b9882b3fad27a0f79a051a9e4
asiek728 <asiek728@gmail.com>
2023-12-20 15:27:23 +0000

    change some styling

--- src/pages/FlashStacksPage/index.jsx
+++ src/pages/FlashStacksPage/index.jsx
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom'
 import axios from 'axios'
 import { FlashStack, StackForm } from '../../components'
 import { useAuthContext } from "../../hooks/useAuthContext"
-
+import './style.css'
 
 const FlashStacksPage = () => {
 
   const [stacks, setStacks] = useState([])
+  // const [filteredStacks, setFilteredStacks] = useState([])
   const [topic, setTopic] = useState("")
+  // const [loading, setLoading] = useState(true);
 
   /////AUTH
   const { user } = useAuthContext()
@@ -23,22 +25,30 @@ const FlashStacksPage = () => {
         //////////////////////////
       })
       setStacks(data)
+      // setLoading(false)
     }
-     ////AUTH
-     if (user) {
-       displayStacks(user)
-     }
-  }, [stacks, user, topic])  ///Need to add user here as dependancy I think (was topic)
+    ////AUTH
+    if (user) {
+      displayStacks(user)
+    }
+  }, [stacks, user, topic]) 
 
+  // useEffect(() => {
+  //   filterHandler()
+  // }, [loading])
+
+  // function filterHandler() {
+  //   setFilteredStacks(stacks.filter(stack => stack.StudentID === user.email))
+  // }
 
   return (
     <>
       <h1>Your flashcard stacks</h1>
-      <StackForm topic={topic} setTopic={setTopic}/>
+      <StackForm topic={topic} setTopic={setTopic} />
       <div className='stacksDiv'>
-        {stacks.map(stack => (
-            <FlashStack stack={stack} key={stack._id} />
-        ))
+        {stacks.map(stack =>
+          <FlashStack stack={stack} key={stack._id} />
+        )
         }
       </div>
 
